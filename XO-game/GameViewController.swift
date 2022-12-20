8ad2143e9b879cb83d200ea82f6b2e7f0949a419
Artem V. Mayer <mayer1art@gmail.com>
2022-12-20 17:48:36 +0700

    add turn state of players

--- XO-game/GameViewController.swift
+++ XO-game/GameViewController.swift
@@ -10,22 +10,62 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    // MARK: - Outlets
+
     @IBOutlet var gameboardView: GameboardView!
     @IBOutlet var firstPlayerTurnLabel: UILabel!
     @IBOutlet var secondPlayerTurnLabel: UILabel!
     @IBOutlet var winnerLabel: UILabel!
     @IBOutlet var restartButton: UIButton!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        gameboardView.onSelectPosition = { [weak self] position in
-            guard let self = self else { return }
-            self.gameboardView.placeMarkView(XView(), at: position)
+
+    // MARK: - Private properties
+
+    private let gameboard = Gameboard()
+    private var currentState: GameState! {
+        didSet {
+            currentState.begin()
         }
     }
-    
-    @IBAction func restartButtonTapped(_ sender: UIButton) {
-        
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setFirstState()
+
+        gameboardView.onSelectPosition = { [weak self] position in
+            guard let self = self
+            else { return }
+
+            self.currentState.addMark(at: position)
+
+            if self.currentState.isCompleted {
+                self.setNextState()
+            }
+        }
+    }
+
+    // MARK: - Actions
+
+    @IBAction func restartButtonTapped(_ sender: UIButton) { }
+
+    // MARK: - Private functions
+
+    private func setFirstState() {
+        currentState = PlayerInputState(player: .first,
+                                        gameViewController: self,
+                                        gameboard: gameboard,
+                                        gameboardView: gameboardView)
+    }
+
+    private func setNextState() {
+        if let playerInputState = currentState as? PlayerInputState {
+            currentState = PlayerInputState(player: playerInputState.player.next,
+                                            gameViewController: self,
+                                            gameboard: gameboard,
+                                            gameboardView: gameboardView)
+        }
     }
 }
 
